830ddd0babf146bbaa9fed3616b4b381034a5305
limjongheok <jjongbbang2@naver.com>
2022-11-30 14:20:06 +0900

    수정

--- src/MainPage/JS/MainGraphLineGraph.js
+++ src/MainPage/JS/MainGraphLineGraph.js
@@ -109,7 +109,7 @@ function MainGraphLineGraph(){
             
           },
           xaxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct','Nov','Dec'],
+            categories: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월','10월','11월','12월'],
           }
         },
       
