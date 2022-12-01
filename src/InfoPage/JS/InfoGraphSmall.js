f9e7b86c1c5285a121e1c4acfe3d71c3baba6c4f
limjongheok <jjongbbang2@naver.com>
2022-12-02 01:29:25 +0900

    색변화

--- src/InfoPage/JS/InfoGraphSmall.js
+++ src/InfoPage/JS/InfoGraphSmall.js
@@ -82,10 +82,10 @@ function InfoGraphSmall(){
                 type:'solid',
                 opacity: [0.4, 1],
                 
-                colors: ['#ceff9d'],
+                colors: ['#cac708'],
         
               },
-            colors :  ['#ceff9d'],
+            colors :  ['#cac708'],
             labels: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월','10월','11월','12월'],
             markers: {
               size: 5
